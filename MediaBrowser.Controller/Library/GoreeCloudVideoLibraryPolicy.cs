a74c62b00491cbe6f8d8adeea797e9e9e48f86aa
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-08-21 21:33:55 -0500

    Add GoreeCloud Video library type policy

---
+++ MediaBrowser.Controller/Library/GoreeCloudVideoLibraryPolicy.cs
@@ -0,0 +1,49 @@
+using System;
+using MediaBrowser.Model.Entities;
+
+namespace MediaBrowser.Controller.Library
+{
+    /// <summary>
+    /// Defines the first-class library collection types supported by GoreeCloud Video.
+    /// </summary>
+    /// <remarks>
+    /// This policy intentionally preserves Jellyfin's inherited collection-type contract while
+    /// preventing non-video library types from being created through GoreeCloud Video's library
+    /// management surface. Shared audio, subtitle, artwork, probing, and transcoding capabilities
+    /// remain available where they are required for video playback.
+    /// </remarks>
+    public static class GoreeCloudVideoLibraryPolicy
+    {
+        /// <summary>
+        /// Determines whether an inherited collection type is supported as a first-class
+        /// GoreeCloud Video library.
+        /// </summary>
+        /// <param name="collectionType">The inherited collection type.</param>
+        /// <returns><see langword="true"/> when the collection type is video-oriented.</returns>
+        public static bool IsSupported(CollectionTypeOptions? collectionType)
+        {
+            return collectionType is CollectionTypeOptions.movies
+                or CollectionTypeOptions.tvshows
+                or CollectionTypeOptions.homevideos
+                or CollectionTypeOptions.mixed;
+        }
+
+        /// <summary>
+        /// Ensures that a requested library collection type is supported by GoreeCloud Video.
+        /// </summary>
+        /// <param name="collectionType">The requested inherited collection type.</param>
+        /// <exception cref="ArgumentException">Thrown when the requested type is not supported.</exception>
+        public static void EnsureSupported(CollectionTypeOptions? collectionType)
+        {
+            if (IsSupported(collectionType))
+            {
+                return;
+            }
+
+            var requestedType = collectionType?.ToString() ?? "unspecified";
+            throw new ArgumentException(
+                $"Collection type '{requestedType}' is not supported by GoreeCloud Video. Supported types are movies, tvshows, homevideos, and mixed.",
+                nameof(collectionType));
+        }
+    }
+}
